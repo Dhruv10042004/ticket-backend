9b99f757dedfea17ed67ee53c0991569c1fc3a2f
Dhruv Nariani <dhruv.nariani@gmail.com>
2025-03-25 18:04:19 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
     allowedHeaders: 'Content-Type,Authorization', // Include 'Authorization' here
   }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://tms.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
